b544538fda513998b8dfa5949a98927409e6a971
Ivan Fonseca <ivanfon@riseup.net>
2019-09-14 12:25:26 -0400

    Move App to separate module

--- src/index.js
+++ src/index.js
@@ -1,41 +1,4 @@
-const bodyParser = require('body-parser');
-const express = require('express');
-const http = require('http');
-const socketio = require('socket.io');
-
-const Database = require('./Database.js');
-const Game = require('./Game.js');
-
-class App {
-  constructor() {
-    // Configure web server
-    this.app = express();
-
-    this.app.use(bodyParser.json());
-
-    // Configure Socket.io server
-    this.server = http.Server(this.app);
-    this.io = socketio(this.server);
-
-    // Configure database
-    this.db = new Database();
-
-    // Setup game
-    this.game = new Game(this.db);
-
-    // Routes
-    this.game.route(this.app);
-
-    // Socket.io
-    this.game.connect(this.io);
-  }
-
-  start(port) {
-    this.server.listen(port, () => {
-      console.log(`Server listening on port ${port}`);
-    });
-  }
-}
+const App = require('./App.js');
 
 const port = process.env.PORT || 8080;
 
