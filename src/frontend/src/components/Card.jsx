7187b4439cf15bfca7be76e6f8d7099610511da5
Ivan Fonseca <ivanfon@riseup.net>
2019-09-15 21:52:12 -0400

    Add graphics to hand/card display

--- src/frontend/src/components/Card.jsx
+++ src/frontend/src/components/Card.jsx
@@ -1,7 +1,36 @@
 import React from 'react';
 
-const Card = ({ type, colour }) => (
-  <p>{colour} {type}</p>
-);
+import '../styles/card.scss';
+
+const Card = ({ type, colour }) => {
+  const typeMapping = {
+    'zero': '0',
+    'one': '1',
+    'two': '2',
+    'three': '3',
+    'four': '4',
+    'five': '5',
+    'six': '6',
+    'seven': '7',
+    'eight': '8',
+    'nine': '9',
+    'skip': '❌',
+    'reverse': '↩️',
+    '+2': '+2',
+    'wild': '🏳️‍🌈',
+    '+4': '+4',
+  };
+
+  return (
+    <div className="card">
+      <div className="card-content">
+        {colour &&
+          <><span className={`colour ${colour}`}></span>&nbsp;</>
+        }
+        <span>{typeMapping[type]}</span>
+      </div>
+    </div>
+  );
+};
 
 export default Card;
--- src/frontend/src/components/Hand.jsx
+++ src/frontend/src/components/Hand.jsx
@@ -2,12 +2,13 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 
 import Card from './Card';
+import '../styles/hand.scss';
 
 const Hand = () => {
   const stateHand = useSelector(state => state.player.hand);
 
   return (
-    <div>
+    <div id="hand">
       {stateHand.map((card, i) => (
         <Card key={i} type={card.type} colour={card.colour} />
       ))}
