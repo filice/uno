84c1dabe7d04cdf467a9df6c37b79542b06f8df0
Ivan Fonseca <ivanfon@riseup.net>
2019-09-16 00:53:02 -0400

    Sort hand by colour

--- src/frontend/src/components/Hand.jsx
+++ src/frontend/src/components/Hand.jsx
@@ -1,15 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 
 import Card from './Card';
 import '../styles/hand.scss';
 
 const Hand = () => {
+  const [sortedHand, setSortedHand] = useState([]);
+
   const stateHand = useSelector(state => state.player.hand);
+
+  // Sort hand
+  useEffect(() => {
+    console.log(stateHand);
+    setSortedHand(
+      stateHand
+        .slice(0)
+        .sort((a, b) => {
+          if (!a.colour) return -1;
+          if (!b.colour) return 1;
+
+          return a.colour.localeCompare(b.colour);
+        })
+    );
+  }, [stateHand]);
 
   return (
     <div id="hand">
-      {stateHand.map((card, i) => (
+      {sortedHand.map((card, i) => (
         <Card key={i} type={card.type} colour={card.colour} />
       ))}
     </div>
